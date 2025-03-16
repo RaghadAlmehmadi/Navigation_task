55e1c2121e47482e55491f9525953576294e9b84
RaghadAlmehmadi <raghadalharbi078@gmail.comraghadalharbi078@gmail.com>
2025-03-16 14:39:46 +0300

    Basic Navigation Setup

---
+++ app/src/main/java/com/example/navigation/Firstscreen.kt
@@ -0,0 +1,45 @@
+package com.example.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Firstscreen(navController: NavController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { navController.navigate("second_screen") },
+                modifier = Modifier
+
+                    .padding(innerPadding)
+
+            ) {
+                Text(text = "Second Screen")
+            }
+        }
+    }
+}
